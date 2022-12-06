304cf314a693f1d8034968145d9ce4495c0d97f3
Gregory Tucker <gregory.tucker@gmail.com>
2022-12-06 07:32:11 +0100

    [Update] skeleton generator

--- src/generate.py
+++ src/generate.py
@@ -1,44 +1,106 @@
 #!/bin/env python
 
-def make_cmakelists(root, project, overwrite=False):
+def make_cmakelists(root, day, project, overwrite=False):
   file = root.joinpath("CMakeLists.txt")
   if overwrite or not file.is_file():
     file.touch()
     with open(file, 'w') as f:
       f.write("cmake_minimum_required(VERSION 3.22.0)\n")
-      f.write(f"project({project})\n")
+      f.write(f"project({project})\n\n")
       f.write("set(SOURCES main.cpp)\n")
       f.write(f"add_executable({project} ${{SOURCES}})\n")
-
       f.write(f"target_compile_options({project} PRIVATE ${{AOC22_CXX_FLAGS}})\n")
-      
       f.write(f"install(TARGETS {project} DESTINATION ${{AOC22_INSTALL_BIN}})\n")
 
+      # write tests first so that trial run last
+      for part in (1, 2):
+        f.write(f"add_test(NAME test.{day}.{part} COMMAND {project} ${{AOC22_INPUT_DIR}}/test.{day} {part} ###)\n")
+      for part in (1, 2):
+        f.write(f"#add_test(NAME {project}.{part} COMMAND {project} ${{AOC22_INPUT_DIR}}/{project} {part})\n")
 
-def make_cpp(root, project, overwrite=False):
+
+CPP_CONTENTS="""
+#include <iostream>
+#include <libaoc.h>
+
+int main(int argc, const char *argv[]){
+  if (argc > 4 || argc < 3) {
+      std::cout << "usage: " << argv[0] << " [filename|input] [part] {[expected result]}" << std::endl;
+    return 1;
+  }
+
+  std::string filename = argv[1];
+  int part = std::stoi(argv[2]);
+  bool do_test = argc > 3;
+  int result = (argc > 3) ? std::stoi(argv[3]) : -1;
+
+  std::fstream fs;
+  fs.open(filename, std::ios::in);
+  auto is_file = fs.is_open();
+
+  std::string input;
+  if (is_file) {
+    getline(fs, input);
+  } else {
+    input = filename;
+  }
+
+  /* Parse input string or file to produce 'answer' */
+
+  std::cout << (do_test ? "Test " : "Part ") << part << " results in " << answer << "\\n";
+
+  /* Compare the expected result and indicate failure for CTest */
+  return (do_test && result != static_cast<int>(answer)) ? 1 : 0;
+}
+"""
+
+
+def make_cpp(root, day, project, overwrite=False):
   file = root.joinpath("main.cpp")
   if overwrite or not file.is_file():
     file.touch()
     with open(file, 'w') as f:
-      f.write("#include <iostream>\n\n")
-      f.write("int main(int argc, const char *argv[]){\n")
-      f.write("if (argc < 2) { std::cout << \"usage information\"; return 1; }\n")
-      f.write("std::cout << argv[0] << \"\\nYour input was:\\n\";")
-      f.write("for (int i=1; i<argc; ++i) std::cout << argv[i] << std::endl;\n")
-      f.write("return 0;\n")
-      f.write("}\n");
+      f.write(f"// Advent of Code day {day} -- binary {project}\n")
+      f.write(CPP_CONTENTS)
 
 
-def main(root, overwrite=False):
-  for d in range(1,26):
+def main(root, overwrite=False, days=None):
+  if days is None:
+    days = range(1, 26)
+  for d in days:
     day = f'{d:02d}'
     daydir = root.joinpath(day)
     if not daydir.is_dir():
       daydir.mkdir()
 
     project = f"aoc22.{day}"
-    make_cmakelists(daydir, project, overwrite=overwrite)
-    make_cpp(daydir, project, overwrite=overwrite)
+    make_cmakelists(daydir, day, project, overwrite=overwrite)
+    make_cpp(daydir, day, project, overwrite=overwrite)
+
+
+# https://stackoverflow.com/a/23535588
+from argparse import Action
+class InflateRange(Action):
+  def __call__(self, parser, namespace, values, option_string=None):
+    from argparse import ArgumentError
+    lst = []
+
+    if len(values) == 0:
+      return None
+
+    for val in values:
+      limits = val.split('-')
+      if len(limits) > 2:
+        raise ArgumentError(self, "%r is not a range of numbers." % part )
+      start = limits[0]
+      if start == '': start = '1'
+      end = limits[-1]
+      try:
+        lst.extend(list(range(int(start), int(end)+1)))
+      except ValueError:
+        raise ArgumentError(self, "%r is not a range of numbers." % part )
+      
+      setattr(namespace, self.dest, lst)
 
 
 if __name__ == '__main__':
@@ -47,8 +109,9 @@ if __name__ == '__main__':
   parser = ArgumentParser(prog='generate.py',
     description="Generate or overwrite the skeleton for the 2022 instance of Advent of Code")
   parser.add_argument('-o', '--overwrite', help='Overwrite CMakeLists.txt and main.cpp files', action='store_true')
+  parser.add_argument("day", help='Day or days to write', action=InflateRange, nargs="*")
   args = parser.parse_args()
 
   root = Path(__file__).resolve().parent
 
-  main(root, overwrite=args.overwrite)
+  main(root, overwrite=args.overwrite, days=args.day)
