584b91d768eb77ef31fd3318a55d09f6cc22f0fb
Gregory Tucker <gregory.tucker@gmail.com>
2022-12-16 12:36:15 +0100

    [Add] Day 16, only test for part 1 working

---
+++ src/16/main.cpp
@@ -0,0 +1,35 @@
+// Advent of Code day 16 -- binary aoc22.16
+// Proboscidea Volcanium
+
+#include <iostream>
+#include <libaoc.h>
+#include "steam.h"
+
+int main(int argc, const char *argv[]){
+  if (argc > 4 || argc < 3) {
+      std::cout << "usage: " << argv[0] << " [filename|input] [part] {[expected result]}" << std::endl;
+    return 1;
+  }
+  using namespace aoc;
+	using namespace aoc::steam;
+
+  std::string filename = argv[1];
+  int part = std::stoi(argv[2]);
+  bool do_test = argc > 3;
+  int result = (argc > 3) ? std::stoi(argv[3]) : -1;
+  int answer = -3;
+
+	auto piperunner = from_strings(read_lines(filename));
+	std::cout << piperunner << "\n";
+
+  if (1 == part){
+		answer = piperunner.optimal_plan(30);
+  }
+
+  /* Parse input string or file to produce 'answer' */
+
+  std::cout << (do_test ? "Test " : "Part ") << part << " results in " << answer << "\n";
+
+  /* Compare the expected result and indicate failure for CTest */
+  return (do_test && result != static_cast<int>(answer)) ? 1 : 0;
+}
---
+++ src/16/steam.cpp
@@ -0,0 +1,55 @@
+#include <regex>
+#include <iostream>
+#include <libaoc.h>
+#include "steam.h"
+
+using namespace aoc::steam;
+
+std::tuple<Cave, std::string, std::string> cave_from_string(const std::string & line){
+  std::regex cave_regex("([A-Z][A-Z])[a-z ]*=([0-9]+);[a-z ]*([A-Z, ]*)");
+	auto cave_begin = std::sregex_iterator(line.begin(), line.end(), cave_regex);
+	auto cave_end = std::sregex_iterator();
+	auto count_info = std::distance(cave_begin, cave_end);
+	
+	if (count_info != 1){
+	  std::string msg = "Regex parsing failed for '";
+		msg += line;
+		msg += "'";
+		throw std::runtime_error(msg);
+	}
+
+	std::smatch match = *cave_begin;
+	std::string name = match.str(1);
+	auto value = static_cast<Valve::val_t>(std::stoi(match.str(2)));
+	std::string outlets = match.str(3);
+
+	auto cave = Cave(std::make_shared<Valve>(name, value));
+	return {cave, name, outlets};
+}
+
+PipeRunner aoc::steam::from_strings(const std::vector<std::string> & lines){
+  std::map<std::string, Cave> caves;
+  std::map<std::string, std::string> outlets;
+  std::vector<std::string> order;
+  order.reserve(lines.size());
+ 
+  for (const auto & line: lines){
+    auto [cave, name, out] = cave_from_string(line);
+ 	  order.push_back(name);
+		caves.emplace(name, cave);
+		outlets.emplace(name, out);
+  }
+  std::vector<Cave> pr;
+	pr.reserve(lines.size());
+  for (const auto & name: order){
+    auto outs = aoc::line_to_columns(outlets.at(name));
+ 	  auto cave = caves.at(name);
+ 	  for (const auto & out: outs) {
+		  cave.add_option(caves.at(out).valve());
+		}
+ 	  pr.push_back(cave);
+  }
+
+	return {std::move(pr)};
+}
+
